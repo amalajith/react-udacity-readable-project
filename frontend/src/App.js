cc7bcf37c330989306d25dfed4ec768c485dc1db
Amal Ajith <pmamalster@gmail.com>
2017-10-14 21:52:41 +0530

    Refactor: Moved categoris api call and posts api call from Home.js to App.js. This will help in rendering the post, if the user refreshes a post detail page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,48 @@
-import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React, {Component} from 'react'
+import {Route, Switch, withRouter} from 'react-router-dom'
+import { connect } from 'react-redux'
 import './App.css';
 import Home from "./containers/home/Home"
 import Posts from "./containers/posts/Posts"
 import PostDetail from "./containers/post-detail/PostDetail"
 import PostCreate from "./containers/post-create/PostCreate"
 import PostEdit from "./containers/post-edit/PostEdit"
+import {getCategoriesFromApi, getPostsFromApi} from "./actions/index"
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          <Switch>
-              <Route exact path='/' component={Home}/>
-              <Route path='/create-post' component={PostCreate} />
-              <Route path='/edit-post/:postId' component={PostEdit}  />
-              <Route exact path='/:category' component={Posts} />
-              <Route path='/:category/:postId' component={PostDetail} />
-          </Switch>
-      </div>
-    )
-  }
+
+    componentDidMount() {
+        this.handleGetCategories()
+        this.handleGetPosts()
+    }
+
+    handleGetCategories = () => {
+        this.props.dispatch(getCategoriesFromApi())
+    }
+
+    handleGetPosts = () => {
+        this.props.dispatch(getPostsFromApi())
+    }
+
+    render() {
+        return (
+            <div>
+                <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/create-post' component={PostCreate}/>
+                    <Route path='/edit-post/:postId' component={PostEdit}/>
+                    <Route exact path='/:category' component={Posts}/>
+                    <Route path='/:category/:postId' component={PostDetail}/>
+                </Switch>
+            </div>
+        )
+    }
 }
 
-export default App;
+const mapStateToProps = ({ categories, posts, sortType}) => ({
+    categories,
+    posts,
+    sortType
+})
+
+export default withRouter(connect(mapStateToProps)(App));
--- frontend/src/containers/home/Home.js
+++ frontend/src/containers/home/Home.js
@@ -2,25 +2,12 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {Container, Grid, Header, Divider} from 'semantic-ui-react'
 import PageHeader from '../../components/page-header/PageHeader'
-import {getCategoriesFromApi, getPostsFromApi} from "../../actions/index"
 import PostCategoryList from "../../components/post-category-list/PostCategoryList"
 import PostList from "../post-list/PostList"
 import CreatePostLink from "../../components/create-post-link/CreatePostLink"
 
 class Home extends Component {
 
-    componentDidMount() {
-        this.handleGetCategories()
-        this.handleGetPosts()
-    }
-
-    handleGetCategories = () => {
-        this.props.dispatch(getCategoriesFromApi())
-    }
-
-    handleGetPosts = () => {
-        this.props.dispatch(getPostsFromApi())
-    }
 
     render() {
 
